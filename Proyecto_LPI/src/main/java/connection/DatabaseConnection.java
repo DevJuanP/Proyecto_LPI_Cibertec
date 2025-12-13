0ba4e944b52bf8740e7715c9d7118534521d798c
Gianfranco Diaz <gianfrancodiaz011@gmail.com>
2025-12-12 20:58:41 -0500

    Renombrar método

--- Proyecto_LPI/src/main/java/connection/DatabaseConnection.java
+++ Proyecto_LPI/src/main/java/connection/DatabaseConnection.java
@@ -12,7 +12,7 @@ public final class DatabaseConnection {
 	private static final String password = "0236195001c413d0e74a09260f24c743dc137858";
 	private static final String url = "jdbc:mysql://"+host+":"+port+"/"+databaseName+"?serverTimezone=America/Lima";
 	
-	public static Connection obtenerConexion() throws SQLException, ClassNotFoundException {
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		return DriverManager.getConnection(url, user, password);
 	}
